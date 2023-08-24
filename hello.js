8325c02ec3f23296fed314ecc01c5ca3b00af5ff
Rog   Montagne <138934686+rogmontagne@users.noreply.github.com>
2023-08-23 20:22:25 -0600

    Se ajustan los mensajes para queden iguales.

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from Costa Rica  pura vida";
+const hello = () => "Hello World from Costa Rica pura vida";
 console.log(hello());
 exports.hello = hello;
--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from Costa Rica  pura vida");
+    expect(hello.hello()).toEqual("Hello World from Costa Rica pura vida");
   });
 });
